427839829efbc316c085e2e33e75efa0c46683a5
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2020-12-28 23:22:00 +0700

    Writing script.js code

--- script.js
+++ script.js
@@ -3,4 +3,29 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
 
+const compareGuesses = (userGuess, computerGuess, target) => {
+  userDifference = Math.abs(userGuess - target);
+  computerDifference = Math.abs(computerGuess - target);
+  return (userDifference <= computerDifference) ? true : false;
+}
+
+const updateScore = (winner) => {
+  switch (winner) {
+    case 'human':
+      humanScore++;
+      break;
+    case 'computer':
+      computerScore++;
+      break;
+  }
+  return undefined;
+}
+
+const advanceRound = () => {
+  currentRoundNumber++;
+  return undefined;
+}
